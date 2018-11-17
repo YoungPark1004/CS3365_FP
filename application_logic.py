feadd9c4f458228685d72f7abdcaf58fc23a2b22
PVorontsov <vorontsovps@gmail.com>
2018-11-16 21:22:07 -0600

    Update application_logic.py

--- application_logic.py
+++ application_logic.py
@@ -1,1 +1,19 @@
 
+class application_logic:
+    
+    processing_item = None
+    
+    def item_information_request(index):
+
+
+          
+    def total_amount_request():
+        
+    
+    def process_payment():
+        
+        
+    def update_inventory(index, quantity, price, description, discount_information):
+        
+    
+    def print_data_report():
