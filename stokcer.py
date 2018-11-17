95083a27dbacce54af7c795d07b0a522208ad2d7
PVorontsov <vorontsovps@gmail.com>
2018-11-16 21:25:42 -0600

    Update stokcer.py

--- stokcer.py
+++ stokcer.py
@@ -1,1 +1,14 @@
 
+class stocker_interface:
+    
+    
+    def prompt_stocker_items():
+        
+        
+    def prompt_stocker_quantity():
+        
+        
+    def display_quantity(quantity):
+        
+        
+    def prompt_stocker_item_information():
