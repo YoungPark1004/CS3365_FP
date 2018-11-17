e1b4f99ba2f3c91ebfa636073931de28c3ddea28
PVorontsov <vorontsovps@gmail.com>
2018-11-16 21:24:15 -0600

    Update inventory.py

--- inventory.py
+++ inventory.py
@@ -1,1 +1,12 @@
 
+class inventory:
+    
+    description = []
+    price = []
+    quantity = []
+    discount_information = []
+    
+    def get_item_information(index):
+        
+    
+    def update_inventory(index, description, price, quantity, discount_information):
