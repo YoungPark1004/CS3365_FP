fc03438ffa38766315fc5b33ec7ca2664b74a07f
PVorontsov <vorontsovps@gmail.com>
2018-11-16 21:26:29 -0600

    Update transaction.py

--- transaction.py
+++ transaction.py
@@ -1,1 +1,8 @@
 
+class transaction:
+    
+    
+    def _init_(self, description, price, quantity, discount_information, customer, total_price, day):
+        
+    
+    def get_item_transaction():
