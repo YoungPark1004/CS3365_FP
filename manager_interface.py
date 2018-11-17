2ea2312ab95d8b24aad8e7a4a9e8c1fba82cbacc
PVorontsov <vorontsovps@gmail.com>
2018-11-16 21:24:41 -0600

    Update manager_interface.py

--- manager_interface.py
+++ manager_interface.py
@@ -1,1 +1,13 @@
 
+class manager_inteface:
+    
+    def prompt_update_item():
+        
+        
+    def prompt_view_item():
+        
+    
+    def display_item_information():
+        
+    
+    def update_information():
