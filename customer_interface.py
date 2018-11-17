0c29b617e5ef809c33968f220ead4a77097f6ff6
PVorontsov <vorontsovps@gmail.com>
2018-11-16 21:23:47 -0600

    Update customer_interface.py

--- customer_interface.py
+++ customer_interface.py
@@ -1,1 +1,33 @@
 
+import application_logic
+
+
+class customer_interface:
+
+
+
+
+
+    def display_item(item):
+        
+
+        
+    def scan_prompt():
+
+        logic = application_logic()
+
+        item = input("enter item")
+
+        item_information = logic.item_information_request
+
+        
+    def select_payment_method():
+        
+        
+        
+        
+    def display_total_amount():
+        
+        
+    
+    def display_receipt()
